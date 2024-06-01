db6dc5960736885766c8e8b804264405cc3e538a
Chunchunmarx <andreeaoprescu12@yahoo.com>
2024-06-01 14:40:05 +0300

    api base

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # loyalty_backend
-test
-test
+
+env   creation: py -m venv ./loyalty
+env activation: source /root/tony/loyalty_backend/envs/loyalty/bin/activate
+
+
+env   creation: py -m venv ./loyalty_test
+env activation: source /root/tony/loyalty_backend/envs/loyalty_test/bin/activate
+
+
+envs:
+loy      - dev
+loy_test - test
---
+++ make_db.py
@@ -0,0 +1,63 @@
+import configparser
+import randomname
+import names
+import random, string
+from flask_api import app, db
+from flask_api.models import User, Manager, Brand
+from flask_test.random_generator import get_random_user, get_random_wallet
+
+'''
+# importing datetime module
+import datetime
+import time
+ 
+# assigned regular string date
+date_time = datetime.datetime(2021, 7, 26, 21, 20)
+ 
+# print regular python date&time
+print("date_time =>",date_time)
+ 
+# displaying unix timestamp after conversion
+unix_timestamp = time.mktime(date_time.timetuple())
+print("unix_timestamp => ", unix_timestamp)
+print(type(unix_timestamp))
+
+
+exit()
+
+app.app_context().push()
+#db.create_all()
+
+user=get_random_user()
+print(user)
+get_random_wallet()
+
+'''
+class DbGenerator():
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    brand_list = []
+
+    def __init__(self):
+        self.config.read('config/models.ini')
+
+    def generate_user(self):
+        return User(
+            nickname = names.get_full_name(),
+            wallet_address = 'G' + ''.join([random.choice(string.ascii_uppercase  + string.digits) for _ in range(int(self.config["common"]["wallet_address_length"]))])
+        )
+
+    def generate_manager(self):
+        return Manager(
+            nickname = names.get_full_name(),
+            wallet_address = 'G' + ''.join([random.choice(string.ascii_uppercase  + string.digits) for _ in range(55)])
+        )
+
+    def generate_brand(self):
+
+        var = ''.join([x.title() for x in randomname.get_name().split('-')])
+        
+        print(var)
+
+db_generator = DbGenerator()
+print(db_generator.generate_user())
+#db_generator.generate_brand()
